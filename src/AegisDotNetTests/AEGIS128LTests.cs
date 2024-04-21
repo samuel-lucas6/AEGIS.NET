0f6c549eef5dfb59b3ad67e3bb14aca9ba0a1555
Samuel Lucas <63159663+samuel-lucas6@users.noreply.github.com>
2024-04-21 17:23:38 +0100

    src: Update tests to .NET 8.

--- src/AegisDotNetTests/AEGIS128LTests.cs
+++ src/AegisDotNetTests/AEGIS128LTests.cs
@@ -11,186 +11,186 @@ public class AEGIS128LTests
     public static IEnumerable<object[]> ValidTestVectors()
     {
         // Test Vector 1
-        yield return new object[]
-        {
+        yield return
+        [
             "c1c0e58bd913006feba00f4b3cc3594eabe0ece80c24868a226a35d16bdae37a",
             "00000000000000000000000000000000",
             "10000200000000000000000000000000",
             "10010000000000000000000000000000",
             "",
             AEGIS128L.MinTagSize
-        };
-        yield return new object[]
-        {
+        ];
+        yield return
+        [
             "c1c0e58bd913006feba00f4b3cc3594e25835bfbb21632176cf03840687cb968cace4617af1bd0f7d064c639a5c79ee4",
             "00000000000000000000000000000000",
             "10000200000000000000000000000000",
             "10010000000000000000000000000000",
             "",
             AEGIS128L.MaxTagSize
-        };
+        ];
         // Test Vector 2
-        yield return new object[]
-        {
+        yield return
+        [
             "c2b879a67def9d74e6c14f708bbcc9b4",
             "",
             "10000200000000000000000000000000",
             "10010000000000000000000000000000",
             "",
             AEGIS128L.MinTagSize
-        };
-        yield return new object[]
-        {
+        ];
+        yield return
+        [
             "1360dc9db8ae42455f6e5b6a9d488ea4f2184c4e12120249335c4ee84bafe25d",
             "",
             "10000200000000000000000000000000",
             "10010000000000000000000000000000",
             "",
             AEGIS128L.MaxTagSize
-        };
+        ];
         // Test Vector 3
-        yield return new object[]
-        {
+        yield return
+        [
             "79d94593d8c2119d7e8fd9b8fc77845c5c077a05b2528b6ac54b563aed8efe84cc6f3372f6aa1bb82388d695c3962d9a",
             "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f",
             "10000200000000000000000000000000",
             "10010000000000000000000000000000",
             "0001020304050607",
             AEGIS128L.MinTagSize
-        };
-        yield return new object[]
-        {
+        ];
+        yield return
+        [
             "79d94593d8c2119d7e8fd9b8fc77845c5c077a05b2528b6ac54b563aed8efe84022cb796fe7e0ae1197525ff67e309484cfbab6528ddef89f17d74ef8ecd82b3",
             "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f",
             "10000200000000000000000000000000",
             "10010000000000000000000000000000",
             "0001020304050607",
             AEGIS128L.MaxTagSize
-        };
+        ];
         // Test Vector 4
-        yield return new object[]
-        {
+        yield return
+        [
             "79d94593d8c2119d7e8fd9b8fc775c04b3dba849b2701effbe32c7f0fab7",
             "000102030405060708090a0b0c0d",
             "10000200000000000000000000000000",
             "10010000000000000000000000000000",
             "0001020304050607",
             AEGIS128L.MinTagSize
-        };
-        yield return new object[]
-        {
+        ];
+        yield return
+        [
             "79d94593d8c2119d7e8fd9b8fc7786f1b80bfb463aba711d15405d094baf4a55a15dbfec81a76f35ed0b9c8b04ac",
             "000102030405060708090a0b0c0d",
             "10000200000000000000000000000000",
             "10010000000000000000000000000000",
             "0001020304050607",
             AEGIS128L.MaxTagSize
-        };
+        ];
         // Test Vector 5
-        yield return new object[]
-        {
+        yield return
+        [
             "b31052ad1cca4e291abcf2df3502e6bdb1bfd6db36798be3607b1f94d34478aa7ede7f7a990fec107542a745733014f9474417b337399507",
             "101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f3031323334353637",
             "10000200000000000000000000000000",
             "10010000000000000000000000000000",
             "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20212223242526272829",
             AEGIS128L.MinTagSize
-        };
-        yield return new object[]
-        {
+        ];
+        yield return
+        [
             "b31052ad1cca4e291abcf2df3502e6bdb1bfd6db36798be3607b1f94d34478aa7ede7f7a990fec10b91e2947a33da8bee89b6794e647baf0fc835ff574aca3fc27c33be0db2aff98",
             "101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f3031323334353637",
             "10000200000000000000000000000000",
             "10010000000000000000000000000000",
             "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20212223242526272829",
             AEGIS128L.MaxTagSize
-        };
+        ];
     }
 
     // https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-aegis-aead#appendix-A.2.7
     public static IEnumerable<object[]> TamperedTestVectors()
     {
         // Test Vector 6
-        yield return new object[]
-        {
+        yield return
+        [
             "79d94593d8c2119d7e8fd9b8fc775c04b3dba849b2701effbe32c7f0fab7",
             "10010000000000000000000000000000",
             "10000200000000000000000000000000",
             "0001020304050607",
             AEGIS128L.MinTagSize
-        };
-        yield return new object[]
-        {
+        ];
+        yield return
+        [
             "79d94593d8c2119d7e8fd9b8fc7786f1b80bfb463aba711d15405d094baf4a55a15dbfec81a76f35ed0b9c8b04ac",
             "10010000000000000000000000000000",
             "10000200000000000000000000000000",
             "0001020304050607",
             AEGIS128L.MaxTagSize
-        };
+        ];
         // Test Vector 7
-        yield return new object[]
-        {
+        yield return
+        [
             "79d94593d8c2119d7e8fd9b8fc785c04b3dba849b2701effbe32c7f0fab7",
             "10000200000000000000000000000000",
             "10010000000000000000000000000000",
             "0001020304050607",
             AEGIS128L.MinTagSize
-        };
-        yield return new object[]
-        {
+        ];
+        yield return
+        [
             "79d94593d8c2119d7e8fd9b8fc7886f1b80bfb463aba711d15405d094baf4a55a15dbfec81a76f35ed0b9c8b04ac",
             "10000200000000000000000000000000",
             "10010000000000000000000000000000",
             "0001020304050607",
             AEGIS128L.MaxTagSize
-        };
+        ];
         // Test Vector 8
-        yield return new object[]
-        {
+        yield return
+        [
             "79d94593d8c2119d7e8fd9b8fc775c04b3dba849b2701effbe32c7f0fab7",
             "10000200000000000000000000000000",
             "10010000000000000000000000000000",
             "0001020304050608",
             AEGIS128L.MinTagSize
-        };
-        yield return new object[]
-        {
+        ];
+        yield return
+        [
             "79d94593d8c2119d7e8fd9b8fc7786f1b80bfb463aba711d15405d094baf4a55a15dbfec81a76f35ed0b9c8b04ac",
             "10000200000000000000000000000000",
             "10010000000000000000000000000000",
             "0001020304050608",
             AEGIS128L.MaxTagSize
-        };
+        ];
         // Test Vector 9
-        yield return new object[]
-        {
+        yield return
+        [
             "79d94593d8c2119d7e8fd9b8fc776c04b3dba849b2701effbe32c7f0fab8",
             "10000200000000000000000000000000",
             "10010000000000000000000000000000",
             "0001020304050607",
             AEGIS128L.MinTagSize
-        };
-        yield return new object[]
-        {
+        ];
+        yield return
+        [
             "79d94593d8c2119d7e8fd9b8fc7786f1b80bfb463aba711d15405d094baf4a55a15dbfec81a76f35ed0b9c8b04ad",
             "10000200000000000000000000000000",
             "10010000000000000000000000000000",
             "0001020304050607",
             AEGIS128L.MaxTagSize
-        };
+        ];
     }
 
     public static IEnumerable<object[]> InvalidParameterSizes()
     {
-        yield return new object[] { AEGIS128L.MinTagSize, 1, AEGIS128L.NonceSize, AEGIS128L.KeySize, 16, AEGIS128L.MinTagSize };
-        yield return new object[] { AEGIS128L.MinTagSize, 0, AEGIS128L.NonceSize + 1, AEGIS128L.KeySize, 16, AEGIS128L.MinTagSize };
-        yield return new object[] { AEGIS128L.MinTagSize, 0, AEGIS128L.NonceSize - 1, AEGIS128L.KeySize, 16, AEGIS128L.MinTagSize };
-        yield return new object[] { AEGIS128L.MinTagSize, 0, AEGIS128L.NonceSize, AEGIS128L.KeySize + 1, 16, AEGIS128L.MinTagSize };
-        yield return new object[] { AEGIS128L.MinTagSize, 0, AEGIS128L.NonceSize, AEGIS128L.KeySize - 1, 16, AEGIS128L.MinTagSize };
-        yield return new object[] { AEGIS128L.MaxTagSize, 0, AEGIS128L.NonceSize, AEGIS128L.KeySize, 16, AEGIS128L.MaxTagSize + 1 };
-        yield return new object[] { AEGIS128L.MaxTagSize, 0, AEGIS128L.NonceSize, AEGIS128L.KeySize, 16, AEGIS128L.MaxTagSize - 1 };
-        yield return new object[] { AEGIS128L.MinTagSize, 0, AEGIS128L.NonceSize, AEGIS128L.KeySize, 16, AEGIS128L.MinTagSize + 1 };
-        yield return new object[] { AEGIS128L.MinTagSize, 0, AEGIS128L.NonceSize, AEGIS128L.KeySize, 16, AEGIS128L.MinTagSize - 1 };
+        yield return [AEGIS128L.MinTagSize, 1, AEGIS128L.NonceSize, AEGIS128L.KeySize, 16, AEGIS128L.MinTagSize];
+        yield return [AEGIS128L.MinTagSize, 0, AEGIS128L.NonceSize + 1, AEGIS128L.KeySize, 16, AEGIS128L.MinTagSize];
+        yield return [AEGIS128L.MinTagSize, 0, AEGIS128L.NonceSize - 1, AEGIS128L.KeySize, 16, AEGIS128L.MinTagSize];
+        yield return [AEGIS128L.MinTagSize, 0, AEGIS128L.NonceSize, AEGIS128L.KeySize + 1, 16, AEGIS128L.MinTagSize];
+        yield return [AEGIS128L.MinTagSize, 0, AEGIS128L.NonceSize, AEGIS128L.KeySize - 1, 16, AEGIS128L.MinTagSize];
+        yield return [AEGIS128L.MaxTagSize, 0, AEGIS128L.NonceSize, AEGIS128L.KeySize, 16, AEGIS128L.MaxTagSize + 1];
+        yield return [AEGIS128L.MaxTagSize, 0, AEGIS128L.NonceSize, AEGIS128L.KeySize, 16, AEGIS128L.MaxTagSize - 1];
+        yield return [AEGIS128L.MinTagSize, 0, AEGIS128L.NonceSize, AEGIS128L.KeySize, 16, AEGIS128L.MinTagSize + 1];
+        yield return [AEGIS128L.MinTagSize, 0, AEGIS128L.NonceSize, AEGIS128L.KeySize, 16, AEGIS128L.MinTagSize - 1];
     }
 
     [TestMethod]
--- src/AegisDotNetTests/AEGIS256Tests.cs
+++ src/AegisDotNetTests/AEGIS256Tests.cs
@@ -11,186 +11,186 @@ public class AEGIS256Tests
     public static IEnumerable<object[]> ValidTestVectors()
     {
         // Test Vector 1
-        yield return new object[]
-        {
+        yield return
+        [
             "754fc3d8c973246dcc6d741412a4b2363fe91994768b332ed7f570a19ec5896e",
             "00000000000000000000000000000000",
             "1000020000000000000000000000000000000000000000000000000000000000",
             "1001000000000000000000000000000000000000000000000000000000000000",
             "",
             AEGIS256.MinTagSize
-        };
-        yield return new object[]
-        {
+        ];
+        yield return
+        [
             "754fc3d8c973246dcc6d741412a4b2361181a1d18091082bf0266f66297d167d2e68b845f61a3b0527d31fc7b7b89f13",
             "00000000000000000000000000000000",
             "1000020000000000000000000000000000000000000000000000000000000000",
             "1001000000000000000000000000000000000000000000000000000000000000",
             "",
             AEGIS256.MaxTagSize
-        };
+        ];
         // Test Vector 2
-        yield return new object[]
-        {
+        yield return
+        [
             "e3def978a0f054afd1e761d7553afba3",
             "",
             "1000020000000000000000000000000000000000000000000000000000000000",
             "1001000000000000000000000000000000000000000000000000000000000000",
             "",
             AEGIS256.MinTagSize
-        };
-        yield return new object[]
-        {
+        ];
+        yield return
+        [
             "6a348c930adbd654896e1666aad67de989ea75ebaa2b82fb588977b1ffec864a",
             "",
             "1000020000000000000000000000000000000000000000000000000000000000",
             "1001000000000000000000000000000000000000000000000000000000000000",
             "",
             AEGIS256.MaxTagSize
-        };
+        ];
         // Test Vector 3
-        yield return new object[]
-        {
+        yield return
+        [
             "f373079ed84b2709faee373584585d60accd191db310ef5d8b11833df9dec7118d86f91ee606e9ff26a01b64ccbdd91d",
             "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f",
             "1000020000000000000000000000000000000000000000000000000000000000",
             "1001000000000000000000000000000000000000000000000000000000000000",
             "0001020304050607",
             AEGIS256.MinTagSize
-        };
-        yield return new object[]
-        {
+        ];
+        yield return
+        [
             "f373079ed84b2709faee373584585d60accd191db310ef5d8b11833df9dec711b7d28d0c3c0ebd409fd22b44160503073a547412da0854bfb9723020dab8da1a",
             "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f",
             "1000020000000000000000000000000000000000000000000000000000000000",
             "1001000000000000000000000000000000000000000000000000000000000000",
             "0001020304050607",
             AEGIS256.MaxTagSize
-        };
+        ];
         // Test Vector 4
-        yield return new object[]
-        {
+        yield return
+        [
             "f373079ed84b2709faee37358458c60b9c2d33ceb058f96e6dd03c215652",
             "000102030405060708090a0b0c0d",
             "1000020000000000000000000000000000000000000000000000000000000000",
             "1001000000000000000000000000000000000000000000000000000000000000",
             "0001020304050607",
             AEGIS256.MinTagSize
-        };
-        yield return new object[]
-        {
+        ];
+        yield return
+        [
             "f373079ed84b2709faee373584588c1cc703c81281bee3f6d9966e14948b4a175b2efbdc31e61a98b4465235c2d9",
             "000102030405060708090a0b0c0d",
             "1000020000000000000000000000000000000000000000000000000000000000",
             "1001000000000000000000000000000000000000000000000000000000000000",
             "0001020304050607",
             AEGIS256.MaxTagSize
-        };
+        ];
         // Test Vector 5
-        yield return new object[]
-        {
+        yield return
+        [
             "57754a7d09963e7c787583a2e7b859bb24fa1e04d49fd550b2511a358e3bca252a9b1b8b30cc4a67ab8a7d53fd0e98d727accca94925e128",
             "101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f3031323334353637",
             "1000020000000000000000000000000000000000000000000000000000000000",
             "1001000000000000000000000000000000000000000000000000000000000000",
             "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20212223242526272829",
             AEGIS256.MinTagSize
-        };
-        yield return new object[]
-        {
+        ];
+        yield return
+        [
             "57754a7d09963e7c787583a2e7b859bb24fa1e04d49fd550b2511a358e3bca252a9b1b8b30cc4a67a3aca270c006094d71c20e6910b5161c0826df233d08919a566ec2c05990f734",
             "101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f3031323334353637",
             "1000020000000000000000000000000000000000000000000000000000000000",
             "1001000000000000000000000000000000000000000000000000000000000000",
             "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20212223242526272829",
             AEGIS256.MaxTagSize
-        };
+        ];
     }
 
     // https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-aegis-aead#appendix-A.3.7
     public static IEnumerable<object[]> TamperedTestVectors()
     {
         // Test Vector 6
-        yield return new object[]
-        {
+        yield return
+        [
             "f373079ed84b2709faee37358458c60b9c2d33ceb058f96e6dd03c215652",
             "1001000000000000000000000000000000000000000000000000000000000000",
             "1000020000000000000000000000000000000000000000000000000000000000",
             "0001020304050607",
             AEGIS256.MinTagSize
-        };
-        yield return new object[]
-        {
+        ];
+        yield return
+        [
             "f373079ed84b2709faee373584588c1cc703c81281bee3f6d9966e14948b4a175b2efbdc31e61a98b4465235c2d9",
             "1001000000000000000000000000000000000000000000000000000000000000",
             "1000020000000000000000000000000000000000000000000000000000000000",
             "0001020304050607",
             AEGIS256.MaxTagSize
-        };
+        ];
         // Test Vector 7
-        yield return new object[]
-        {
+        yield return
+        [
             "f373079ed84b2709faee37358459c60b9c2d33ceb058f96e6dd03c215652",
             "1000020000000000000000000000000000000000000000000000000000000000",
             "1001000000000000000000000000000000000000000000000000000000000000",
             "0001020304050607",
             AEGIS256.MinTagSize
-        };
-        yield return new object[]
-        {
+        ];
+        yield return
+        [
             "f373079ed84b2709faee373584598c1cc703c81281bee3f6d9966e14948b4a175b2efbdc31e61a98b4465235c2d9",
             "1000020000000000000000000000000000000000000000000000000000000000",
             "1001000000000000000000000000000000000000000000000000000000000000",
             "0001020304050607",
             AEGIS256.MaxTagSize
-        };
+        ];
         // Test Vector 8
-        yield return new object[]
-        {
+        yield return
+        [
             "f373079ed84b2709faee37358458c60b9c2d33ceb058f96e6dd03c215652",
             "1000020000000000000000000000000000000000000000000000000000000000",
             "1001000000000000000000000000000000000000000000000000000000000000",
             "0001020304050608",
             AEGIS256.MinTagSize
-        };
-        yield return new object[]
-        {
+        ];
+        yield return
+        [
             "f373079ed84b2709faee373584588c1cc703c81281bee3f6d9966e14948b4a175b2efbdc31e61a98b4465235c2d9",
             "1000020000000000000000000000000000000000000000000000000000000000",
             "1001000000000000000000000000000000000000000000000000000000000000",
             "0001020304050608",
             AEGIS256.MaxTagSize
-        };
+        ];
         // Test Vector 9
-        yield return new object[]
-        {
+        yield return
+        [
             "f373079ed84b2709faee37358458c60b9c2d33ceb058f96e6dd03c215653",
             "1000020000000000000000000000000000000000000000000000000000000000",
             "1001000000000000000000000000000000000000000000000000000000000000",
             "0001020304050607",
             AEGIS256.MinTagSize
-        };
-        yield return new object[]
-        {
+        ];
+        yield return
+        [
             "f373079ed84b2709faee373584588c1cc703c81281bee3f6d9966e14948b4a175b2efbdc31e61a98b4465235c2da",
             "1000020000000000000000000000000000000000000000000000000000000000",
             "1001000000000000000000000000000000000000000000000000000000000000",
             "0001020304050607",
             AEGIS256.MaxTagSize
-        };
+        ];
     }
 
     public static IEnumerable<object[]> InvalidParameterSizes()
     {
-        yield return new object[] { AEGIS256.MinTagSize, 1, AEGIS256.NonceSize, AEGIS256.KeySize, 16, AEGIS256.MinTagSize };
-        yield return new object[] { AEGIS256.MinTagSize, 0, AEGIS256.NonceSize + 1, AEGIS256.KeySize, 16, AEGIS256.MinTagSize };
-        yield return new object[] { AEGIS256.MinTagSize, 0, AEGIS256.NonceSize - 1, AEGIS256.KeySize, 16, AEGIS256.MinTagSize };
-        yield return new object[] { AEGIS256.MinTagSize, 0, AEGIS256.NonceSize, AEGIS256.KeySize + 1, 16, AEGIS256.MinTagSize };
-        yield return new object[] { AEGIS256.MinTagSize, 0, AEGIS256.NonceSize, AEGIS256.KeySize - 1, 16, AEGIS256.MinTagSize };
-        yield return new object[] { AEGIS256.MaxTagSize, 0, AEGIS256.NonceSize, AEGIS256.KeySize, 16, AEGIS256.MaxTagSize + 1 };
-        yield return new object[] { AEGIS256.MaxTagSize, 0, AEGIS256.NonceSize, AEGIS256.KeySize, 16, AEGIS256.MaxTagSize - 1 };
-        yield return new object[] { AEGIS256.MinTagSize, 0, AEGIS256.NonceSize, AEGIS256.KeySize, 16, AEGIS256.MinTagSize + 1 };
-        yield return new object[] { AEGIS256.MinTagSize, 0, AEGIS256.NonceSize, AEGIS256.KeySize, 16, AEGIS256.MinTagSize - 1 };
+        yield return [AEGIS256.MinTagSize, 1, AEGIS256.NonceSize, AEGIS256.KeySize, 16, AEGIS256.MinTagSize];
+        yield return [AEGIS256.MinTagSize, 0, AEGIS256.NonceSize + 1, AEGIS256.KeySize, 16, AEGIS256.MinTagSize];
+        yield return [AEGIS256.MinTagSize, 0, AEGIS256.NonceSize - 1, AEGIS256.KeySize, 16, AEGIS256.MinTagSize];
+        yield return [AEGIS256.MinTagSize, 0, AEGIS256.NonceSize, AEGIS256.KeySize + 1, 16, AEGIS256.MinTagSize];
+        yield return [AEGIS256.MinTagSize, 0, AEGIS256.NonceSize, AEGIS256.KeySize - 1, 16, AEGIS256.MinTagSize];
+        yield return [AEGIS256.MaxTagSize, 0, AEGIS256.NonceSize, AEGIS256.KeySize, 16, AEGIS256.MaxTagSize + 1];
+        yield return [AEGIS256.MaxTagSize, 0, AEGIS256.NonceSize, AEGIS256.KeySize, 16, AEGIS256.MaxTagSize - 1];
+        yield return [AEGIS256.MinTagSize, 0, AEGIS256.NonceSize, AEGIS256.KeySize, 16, AEGIS256.MinTagSize + 1];
+        yield return [AEGIS256.MinTagSize, 0, AEGIS256.NonceSize, AEGIS256.KeySize, 16, AEGIS256.MinTagSize - 1];
     }
 
     [TestMethod]
